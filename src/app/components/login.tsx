99420d93407557481282a03ed5f9b7abefd802cb
wyx\王宇瑄 <1343700180@qq.com>
2025-01-03 16:41:16 -0500

    Home page's style

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -7,8 +7,9 @@ const signinButtion: React.FC = () => {
     return (
         <Button
             variant="contained"
+            className='px-6 py-3 text-white rounded-lg shadow-md hover:bg-blue-700 w-64 h-16 text-xl'
         >
-            <Link href={'/auth/signin'}>Sign In</Link>
+            <Link href={'/auth/signin'}>Get Started</Link>
         </Button>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 import Image from "next/image";
 import Login from '@/app/components/login';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Home: React.FC = () => {
-    const title = 'A simple to do list\nto manage you life'
+    const title = 'A simple to do list\nto manage you life!'
 
     return (
         <div className="">
-            <header>
-
+            <header className="h-[50px] border-b border-gray-400 bg-gray-50 flex items-center justify-between pr-8 pl-8">
+                <div className="todo-logo">TODO</div>
+                <AccountCircleIcon className="h-[40px] w-[40px]"/>
             </header>
-            <main className="">
+            <main className="flex w-full h-[calc(100vh-50px)] justify-center items-center bg-gray-50">
                 <Image
                     aria-hidden
                     src="/Work.svg"
                     alt="File icon"
-                    width={160}
-                    height={160}
+                    width={500}
+                    height={500}
                 />
-                <div>
-                    <pre>
+                <div className="flex flex-col items-center h-[500px] justify-evenly ml-7">
+                    <pre className="text-5xl font-bold text-gray-800 leading-snug font-serif">
                         {title}
                     </pre>
                     <Login></Login>
